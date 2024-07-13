3337979c16e5bec0e26c24cae6f3bf95123b0f8d
Mike Bell <hello@mikebell.io>
2024-07-13 17:32:49 +0100

    Initial commit

---
+++ devenv.nix
@@ -0,0 +1,49 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ 
+    pkgs.git
+    pkgs.jq
+    pkgs.curl
+    pkgs.kubectl
+    pkgs.kubectx
+    pkgs.starship
+    pkgs.tmux
+  ];
+
+  # Starship conf
+  starship.config.path = ./config/starship.toml;
+  starship.config.enable = true;
+  starship.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
